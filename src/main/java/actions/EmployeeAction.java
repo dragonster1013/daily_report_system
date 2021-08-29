5bdf79020b572dae1ec93f61eff7a5b5b33a917b
ryusei.yoshimura <yoshidoraster1013@gmail.com>
2021-08-29 20:58:45 +0900

    管理者チェックを追加

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -26,7 +26,7 @@ public class EmployeeAction extends ActionBase {
      */
     @Override
     public void process() throws ServletException, IOException {
-
+        
         service = new EmployeeService();
 
         //メソッドを実行
@@ -41,6 +41,9 @@ public class EmployeeAction extends ActionBase {
      * @throws IOException
      */
     public void index() throws ServletException, IOException {
+        
+        //管理者かどうかチェック //追記
+        if (checkAdmin()) { //追記
 
         //指定されたページ数の一覧画面に表示するデータを取得
         int page = getPage();
@@ -63,7 +66,9 @@ public class EmployeeAction extends ActionBase {
 
         //一覧画面を表示
         forward(ForwardConst.FW_EMP_INDEX);
-
+        
+        }//追記
+        
     }
 
     /**
@@ -72,12 +77,18 @@ public class EmployeeAction extends ActionBase {
      * @throws IOException
      */
     public void entryNew() throws ServletException, IOException {
+        
+        //管理者かどうかチェック
+        if (checkAdmin()) {
 
         putRequestScope(AttributeConst.TOKEN, getTokenId()); //CSRF対策用トークン
         putRequestScope(AttributeConst.EMPLOYEE, new EmployeeView()); //空の従業員インスタンス
 
         //新規登録画面を表示
         forward(ForwardConst.FW_EMP_NEW);
+        
+        }//追記
+        
     }
 
     /**
@@ -86,9 +97,9 @@ public class EmployeeAction extends ActionBase {
      * @throws IOException
      */
     public void create() throws ServletException, IOException {
-
+        
         //CSRF対策 tokenのチェック
-        if (checkToken()) {
+        if (checkToken() && checkToken()) {//追記
 
             //パラメータの値を元に従業員情報のインスタンスを作成する
             EmployeeView ev = new EmployeeView(
@@ -136,6 +147,9 @@ public class EmployeeAction extends ActionBase {
      * @throws IOException
      */
     public void show() throws ServletException, IOException {
+        
+        //管理者かどうかのチェック //追記
+        if (checkAdmin()) {//追記
 
         //idを条件に従業員データを取得する
         EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
@@ -151,6 +165,9 @@ public class EmployeeAction extends ActionBase {
 
         //詳細画面を表示
         forward(ForwardConst.FW_EMP_SHOW);
+        
+        }
+        
     }
 
     /**
@@ -159,6 +176,9 @@ public class EmployeeAction extends ActionBase {
      * @throws IOException
      */
     public void edit() throws ServletException, IOException {
+        
+        //管理者かどうかのチェック
+        if (checkAdmin()) {
 
         //idを条件に従業員データを取得する
         EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
@@ -175,7 +195,8 @@ public class EmployeeAction extends ActionBase {
 
         //編集画面を表示する
         forward(ForwardConst.FW_EMP_EDIT);
-
+        
+        }//追記
     }
 
     /**
@@ -184,7 +205,7 @@ public class EmployeeAction extends ActionBase {
      * @throws IOException
      */
     public void update() throws ServletException, IOException {
-
+        
         //CSRF対策 tokenのチェック
         if (checkToken()) {
             //パラメータの値を元に従業員情報のインスタンスを作成する
@@ -244,6 +265,30 @@ public class EmployeeAction extends ActionBase {
             //一覧画面にリダイレクト
             redirect(ForwardConst.ACT_EMP, ForwardConst.CMD_INDEX);
         }
+    }
+
+    /**
+     * ログイン中の従業員が管理者かどうかチェックし、管理者でなければエラー画面を表示
+     * true: 管理者 false: 管理者ではない
+     * @throws ServletException
+     * @throws IOException
+     */
+    private boolean checkAdmin() throws ServletException, IOException {
+
+        //セッションからログイン中の従業員情報を取得
+        EmployeeView ev = (EmployeeView) getSessionScope(AttributeConst.LOGIN_EMP);
+
+        //管理者でなければエラー画面を表示
+        if (ev.getAdminFlag() != AttributeConst.ROLE_ADMIN.getIntegerValue()) {
+
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+            return false;
+
+        } else {
+
+            return true;
+        }
+
     }
 
 }
