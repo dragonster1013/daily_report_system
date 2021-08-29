7a8e29e44260d7e72a649ba031ecef756aa14b8b
ryusei.yoshimura <yoshidoraster1013@gmail.com>
2021-08-29 15:02:49 +0900

    定数ファイルを作成

--- src/main/java/constants/MessageConst.java
+++ src/main/java/constants/MessageConst.java
@@ -6,23 +6,24 @@ package constants;
  */
 public enum MessageConst {
 
-    // 認証
-    I_LOGINED("ログインしました。"),
+    //認証
+    I_LOGINED("ログインしました"),
     E_LOGINED("ログインに失敗しました。"),
-    I_LOGOUT("ログアウトしました"),
+    I_LOGOUT("ログアウトしました。"),
 
-    // DB更新
+    //DB更新
     I_REGISTERED("登録が完了しました。"),
     I_UPDATED("更新が完了しました。"),
     I_DELETED("削除が完了しました。"),
 
-    // バリデーション
+    //バリデーション
     E_NONAME("氏名を入力してください。"),
     E_NOPASSWORD("パスワードを入力してください。"),
     E_NOEMP_CODE("社員番号を入力してください。"),
     E_EMP_CODE_EXIST("入力された社員番号の情報は既に存在しています。"),
     E_NOTITLE("タイトルを入力してください。"),
     E_NOCONTENT("内容を入力してください。");
+
 
     /**
      * 文字列
@@ -36,7 +37,7 @@ public enum MessageConst {
         this.text = text;
     }
 
-    /*
+    /**
      * 値(文字列)取得
      */
     public String getMessage() {
