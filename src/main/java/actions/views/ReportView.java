95f7e5cbe4a6d1c27b2d37cfca7c7fdda35e8605
ryusei.yoshimura <yoshidoraster1013@gmail.com>
2021-08-31 19:10:15 +0900

    Reportモデル、ReportViewモデル作成

--- src/main/java/actions/views/ReportView.java
+++ src/main/java/actions/views/ReportView.java
@@ -9,13 +9,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
- * 日報情報について
+ * 日報情報について画面の入力値・出力値を扱うViewモデル
  *
  */
 @Getter //全てのクラスフィールドについてgetterを自動生成する(Lombok)
-@Setter //全てのクラスフィ―ルドについてsetterを自動生成する(Lombok)
+@Setter //全てのクラスフィールドについてsetterを自動生成する(Lombok)
 @NoArgsConstructor //引数なしコンストラクタを自動生成する(Lombok)
-@AllArgsConstructor //全てのクラスフィールドを引数にもつコンストラクタを自動生成する(Lombok)
+@AllArgsConstructor //全てのクラスフィールドを引数にもつ引数ありコンストラクタを自動生成する(Lombok)
 public class ReportView {
 
     /**
@@ -41,15 +41,16 @@ public class ReportView {
     /**
      * 日報の内容
      */
-    private String conent;
+    private String content;
 
     /**
      * 登録日時
      */
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
 
     /**
      * 更新日時
      */
     private LocalDateTime updatedAt;
+
 }
