b20797abb8d89b5e4a076260154d9594d5dfadef
ryusei.yoshimura <yoshidoraster1013@gmail.com>
2021-09-02 00:07:25 +0900

    Checkモデル、CheckViewモデル作成

---
+++ src/main/java/actions/views/CheckView.java
@@ -0,0 +1,44 @@
+package actions.views;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 確認サイン情報について画面の入力値・出力値を扱うViewモデル
+ *
+ */
+@Getter //全てのクラスフィールドについてgetterを自動生成する(Lombok)
+@Setter //全てのクラスフィールドについてsetterを自動生成する(Lombok)
+@NoArgsConstructor //引数なしコンストラクタを自動生成する(Lombok)
+@AllArgsConstructor //全てのクラスフィールドを引数にもつ引数ありコンストラクタを自動生成する(Lombok)
+public class CheckView {
+
+    /**
+     * id
+     */
+    private Integer id;
+
+    /**
+     * 登録した日報
+     */
+    private ReportView report;
+
+    /**
+     * 確認サイン
+     */
+    private String confirm;
+
+    /**
+     * 登録日時
+     */
+    private LocalDateTime createdAt;
+
+    /**
+     * 更新日時
+     */
+    private LocalDateTime updatedAt;
+}
